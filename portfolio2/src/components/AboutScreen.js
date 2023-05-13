cd388c417b79eb7c251145d94860bcf2c6571b8c
prajwalshah19 <pjpackers99@gmail.com>
2023-05-13 11:22:03 -0500

    everything is back now

---
+++ portfolio2/src/components/AboutScreen.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import '../App.css'
+
+export function AboutScreen(props) {
+
+    return (
+        <div
+        className = "homeScreen"
+        >
+            <h1>About Me</h1>
+            <div
+            className = "bio"
+            >
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Integer eget aliquet nibh praesent tristique magna sit amet. Sit amet luctus venenatis lectus magna fringilla. Viverra mauris in aliquam sem. Ut tortor pretium viverra suspendisse potenti. </p>
+            </div>
+            <div
+            className = "education"
+            >
+                <h2>Education</h2>
+            </div>
+            <div
+            className = "experience"
+            >
+                <h2>Experience</h2>
+                
+            </div>
+            <div
+            className = "buttonCon"
+            >
+
+            </div>
+
+
+
+
+        </div>
+            
+            
+
+        );
+
+}
---
+++ portfolio2/src/components/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../App.css'
+
+export function Contact(props) {
+
+    return (
+        <div>
+
+
+        </div>
+            
+            
+
+        );
+
+}

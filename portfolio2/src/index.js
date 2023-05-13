f057692b2d669b086e49777c8e559256dac5999e
prajwalshah19 <pjpackers99@gmail.com>
2023-05-13 10:28:44 -0500

    trying new way to find root

--- portfolio2/src/index.js
+++ portfolio2/src/index.js
@@ -5,7 +5,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { HashRouter as Router } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+
 root.render(
   <React.StrictMode>
 

f095d8a5bb3fd1ba97baf5c0357fffe2acaee66a
prajwalshah19 <pjpackers99@gmail.com>
2023-05-13 10:26:29 -0500

    changes

--- portfolio2/src/index.js
+++ portfolio2/src/index.js
@@ -8,9 +8,9 @@ import { HashRouter as Router } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router >
+
       <App />
-    </Router>
+
   </React.StrictMode>
 );
 
